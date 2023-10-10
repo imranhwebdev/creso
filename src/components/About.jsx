2fe60cc41974b30226fad846e622b14bfef94f34
imran <alimrancv870@gmail.com>
2023-10-10 22:35:30 +0600

    thirdcommit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,28 @@
 import {React} from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
+import Aboutleftimg from '../assets/img/about-left-img.png'
+import Aboutcontentimg from '../assets/img/about-content-img.png'
+import AboutRightArrowimg from '../assets/img/about-right-arrow.png'
 export default function About(){
     return(
         <div className="about" id='about'>
             <Container>
                 <div className="about-wrapper">
                     <Row className='align-items-center'>
-                        <Col lg={12}>
-                            <div className="banner-content">
-                              
+                        <Col lg={6}>
+                            <div className="about-left">
+                              <figure>
+                                <img src={Aboutleftimg} alt="" />
+                              </figure>
+                            </div>
+                        </Col>
+                        <Col lg={6}>
+                            <div className="about-content">
+                                <figure>
+                                    <img src={AboutRightArrowimg} alt="" />
+                                </figure>
+                              <h2>Seedless <span>Login <img src={Aboutcontentimg} alt="" /></span></h2>
+                              <p>Create and login to your wallet seamlessly with your Google, Twitter, or Apple account—no mnemonic fuss, just simple, swift access!</p>
                             </div>
                         </Col>
                     </Row>
--- src/components/Socialrecovery.jsx
+++ src/components/Socialrecovery.jsx
@@ -1,17 +1,32 @@
 import {React} from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
+import Socialrightimg1 from '../assets/img/social-right-img1.png'
+import Socialrightimg2 from '../assets/img/social-right-img-2.png'
+import socialleftimg from '../assets/img/social-left-img.png'
 export default function Socialrecovery(){
     return(
         <div className="socialrecovery" id='socialrecovery'>
-                <Container>
-                    <Row className='align-items-center'>
-                        <Col lg={12}>
-                            <div className="socialrecovery-content">
-                               
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
+            <div className="socialrecovery-left-img">
+                <figure>
+                    <img src={socialleftimg} alt="" />
+                </figure>
+            </div>
+            <Container>
+                <Row className='align-items-center'>
+                    <Col lg={5}  lg={{offset: 7 }} >
+                        <div className="socialrecovery-content">
+                            <figure className='img1'>
+                                <img src={Socialrightimg1} alt="" />
+                            </figure>
+                            <figure className='img2'>
+                                <img src={Socialrightimg2} alt="" />
+                            </figure>
+                            <h2>Gasless <span>Transactions</span></h2>
+                            <p>Use any token to manage gas fees. Relish the ease as an App or a friend takes care of the gas, turning every transaction into a smooth sail.</p>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
