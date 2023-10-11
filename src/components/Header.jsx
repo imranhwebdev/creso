8e4b4a9882c7fe72d6be587fc4203151deefd46f
imran <alimrancv870@gmail.com>
2023-10-11 21:34:13 +0600

    headerupdatedone

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,11 @@ import Navbar from 'react-bootstrap/Navbar';
 import logo from '../assets/img/site-logo.svg'
 import { Link, animateScroll as scroll} from 'react-scroll';
 import {FaShoppingBag} from "react-icons/fa";
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Dropdown from 'react-bootstrap/Dropdown';
+import Eth from '../assets/img/eth-logo.svg'
+import Twitter from '../assets/img/twitter.svg'
+import Telegram from '../assets/img/telegram.svg'
+import Discord from '../assets/img/descord.svg'
 
 export default function Header(){
     const [fix, setFix] = useState(false)
@@ -35,12 +39,17 @@ export default function Header(){
                         </Nav>
                     </Navbar.Collapse>
                     <div className='header-buttons d-flex align-items-center'>
-                            <NavDropdown title="ETH" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">ETH</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2"> Twitter (X)</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Telegram</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Discord</NavDropdown.Item>
-                            </NavDropdown>
+                            <Dropdown>
+                                <Dropdown.Toggle>
+                                <img src={Eth} alt="" /> ETH
+                                </Dropdown.Toggle>
+
+                                <Dropdown.Menu>
+                                    <Dropdown.Item href="#/action-1"> <img src={Twitter} alt="" /> Twitter (X)</Dropdown.Item>
+                                    <Dropdown.Item href="#/action-2"><img src={Telegram} alt="" /> Telegram</Dropdown.Item>
+                                    <Dropdown.Item href="#/action-3"><img src={Discord} alt="" /> Discord</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
                         <a href="/" className='base-btn'>Chart</a>
                         <a href="/" className='base-btn active Buy'><FaShoppingBag /><span> Buy Creso</span></a>
                     </div>
